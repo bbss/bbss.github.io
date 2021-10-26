32c53667ed46b1baec5f7308311b1119f6a4c3ae
Baruch Berger <baruchberger@gmail.com>
2021-10-26 14:32:08 +0900

    app

--- resources/public/js/main/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { bbss.portfolio.init(); } catch (e) { console.error("An error occurred when calling (bbss.portfolio/init)"); throw(e); }
